21bd8548870741531ad2915936025948ee80e288
varunrocxx <varungupta117@gmail.com>
2019-12-02 20:38:52 +0530

    New and better Layout

--- main.py
+++ main.py
@@ -1,35 +1,79 @@
 import tkinter as tk
 from PIL import ImageTk,Image
+import time
 
-w = 500
-h = 250
+#Setting Dimensions of the window
 
-root = tk.Tk()
+w = 700
+h = 350
 
-#============================
-
-root.wm_attributes('-transparentcolor','white')
-
-#============================
-
-
-root.geometry('%dx%d+0+0' % (w,h))
+#  Loading Image 
 
 image = Image.open("index.jpg")
 image = image.resize((w,h))
-photo = ImageTk.PhotoImage(image)
 
-#canvas  = tk.Canvas(root,width=500 , height=250 , bg ='blue').place(relx= 0,rely = 0, relwidth = 1 , relheight = 1)
-frame = tk.Frame(root,bg="red").place(relx= 0,rely = 0, relwidth = 1 , relheight = 1)
 
-bg_label = tk.Label(root,image=photo).place(relx= 0,rely = 0, relwidth = 1 , relheight = 1)
 
-lbl1 = tk.Label(frame,text="Time Remaining :",bg='black',font=("forte",30))
-lbl1.pack(side="top")
-#Button 1
-tk.Button(frame,text="Stop").place(relx= 0,rely = 0.1, relwidth = 0.3 , relheight = 0.5)
+class main():
 
-root.resizable(False,False)
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry('%dx%d+0+0' % (w,h))
+        self.photo = ImageTk.PhotoImage(image)
+        self.bg_img = tk.Label(self.root,image = self.photo)
+        self.root.resizable(False,False)
+        self.lbl1 = tk.Label(self.root,text="Time",font=('forte',20)) 
+        self.t = tk.StringVar()
+        self.t.set("00:00:00")
+        self.time = tk.Label(self.root,textvariable=self.t,font=('forte',30))
+        self.libo = tk.Listbox(self.root,height=3)
+        self.libo.insert(1,"20-20")
+        self.libo.insert(2,"30-20")
+        self.libo.insert(1,"30-30")
+        print(self.libo.curselection())
 
-root.mainloop()
+        self.libo.pack(side="bottom")
+        
+        self.bg_img.place(relwidth=1,relheight=1)
+        self.lbl1.place(relx= 0.03,rely = 0.05, relwidth = 0.3 , relheight = 0.2 )
+        self.time.place(relx= 0.35,rely = 0.05, relwidth = 0.4 , relheight = 0.2)
 
+        self.root.mainloop()
+
+
+# root = tk.Tk()
+
+# #============================
+
+# root.wm_attributes('-transparentcolor','red')
+
+# #============================
+# i=0
+# def change():
+#     global i
+#     i = i+1
+#     t.set("yeh"+str(i))
+#     root.after(960,change)
+    
+
+# t = tk.StringVar()
+# t.set("00:00:00")
+
+# root.geometry('%dx%d+0+0' % (w,h))
+
+
+# #canvas  = tk.Canvas(root,width=500 , height=250 , bg ='blue').place(relx= 0,rely = 0, relwidth = 1 , relheight = 1)
+# frame = tk.Frame(root,bg="red").place(relx= 0,rely = 0, relwidth = 1 , relheight = 1)
+
+# bg_label = tk.Label(root,image=photo).place(relx= 0,rely = 0, relwidth = 1 , relheight = 1)
+
+# lbl1 = tk.Label(root,text="Time Remaining :",font=("forte",15))
+# lbl1.place(relx= 0.05,rely = 0.05, relwidth = 0.3 , relheight = 0.2)
+# #Button 1
+# bt1 = tk.Button(frame,textvariable=t,command= lambda:change()).place(relx= 0.5,rely = 0.1, relwidth = 0.3 , relheight = 0.1)
+
+# root.resizable(False,False)
+
+# root.mainloop()
+
+a = main()
